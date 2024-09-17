278726694edbec666d58443c446a45e6be46b60d
Franco Ruis Dias <franco.dias@datumsure.com>
2024-09-17 09:40:08 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'storage',
+  webDir: 'www'
+};
+
+export default config;
